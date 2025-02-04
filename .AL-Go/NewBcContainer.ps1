78a0f066587b1f8f4186c7d837923b866f09d675
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2025-02-04 22:19:29 +0100

    Update NewBcContainer.ps1

--- .AL-Go/NewBcContainer.ps1
+++ .AL-Go/NewBcContainer.ps1
@@ -2,5 +2,5 @@ Param(
     [Hashtable]$parameters
 )
 
-$parameters.additionalParameters += @("--env customNavSettings=ClrRetrieverKind=OneApplicationObjectOneAssembly")
+#$parameters.additionalParameters += @("--env customNavSettings=ClrRetrieverKind=OneApplicationObjectOneAssembly")
 New-BcContainer @parameters
