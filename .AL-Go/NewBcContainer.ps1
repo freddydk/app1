d29854205c417fff2745d789eb9485da6d5015cc
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2025-02-04 19:15:16 +0100

    Update NewBcContainer.ps1

--- .AL-Go/NewBcContainer.ps1
+++ .AL-Go/NewBcContainer.ps1
@@ -2,5 +2,6 @@ Param(
     [Hashtable]$parameters
 )
 
+$parameters | out-host
 $parameters.multitenant = $false
-New-BcContainer @parameters
+New-BcContainer @parameters -additionalParameters @("--env customNavSettings=ClrRetrieverKind=OneApplicationObjectOneAssembly")
